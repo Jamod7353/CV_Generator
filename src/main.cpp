c85e3206652a71e19489a3a8da96d7e307ada76f
Jamod7353 <hasensaft@gmx.de>
2022-12-21 16:58:11 +0100

    2 channels

--- src/main.cpp
+++ src/main.cpp
@@ -6,55 +6,42 @@
 #include "scales.h"
 #include "settings.h"
 
-#define MCP4725_1 0x60
-#define MCP4725_2 0x61 //TODO 60 oder 62?
+#define MCP4725_0 0x60
+#define MCP4725_1 0x61 //TODO 60 oder 62?
+Adafruit_MCP4725 dac0;
 Adafruit_MCP4725 dac1;
-Adafruit_MCP4725 dac2;
 byte buffer[3];
-Encoder enc(PIN_ENC_1, PIN_ENC_2);
+Encoder enc(PIN_ENC_0, PIN_ENC_1);
 long encPosition = -999;
 
-byte range_semitones;
-#define NUM_OF_STEPS 16
-byte random_steps[NUM_OF_STEPS];
-byte random_step_pointer = 0;
-int possibility; // value 0-1023
-byte scale_pointer = 0;
-scale picked_scale = scales[0];
-
-#define AVG_LENGTH 8
-unsigned int range_AVG[AVG_LENGTH];
-byte range_AVG_pointer = 0;
-unsigned int possibility_AVG[AVG_LENGTH];
-byte possibility_AVG_pointer = 0;
+#define NUM_OF_STEPS 16  // Array length for random/record
+byte range_semitones[2] = {NUM_OF_STEPS, NUM_OF_STEPS};
+byte random_steps0[NUM_OF_STEPS];
+byte random_steps1[NUM_OF_STEPS];
+byte random_step_pointer[2] = {0, 0};
+int possibility[2] = {0, 0}; // value 0-1023
+byte scale_pointer[2] = {0, 0};
+scale picked_scale[2] = {scales[0], scales[0]};
 
 #define UP 0
 #define DOWN 1
 #define RANDOM 2
-#define INPUT 3
+#define SLOWLY_UP 3
+#define SLOWLY_DOWN 4
+#define SLOWLY_BOUNCE 5
+#define INPUT 6
 // TODO: more modes (slightly up/down (random -2 to +3))
-byte play_mode;
+byte play_mode[2];
 
 #define CLK_MODE 0
 #define TRIG_MODE 1
-byte timer_mode;
-boolean stepMarked1 = false;
-boolean stepMarked2 = false;
-long timeForNextStep;
+byte timer_mode[2] = {1, 1}; // TODO: Wert initialisieren und hier löschen
+boolean stepMarked[2] = {false, false};
 
+long timeForNextStep;
 int millisToInterrupt = 462; // 60000/bpm (130 bpm)
 
-byte value_tone = 0; //calculated value -> to scale in volt: value*1/12
-
-unsigned int avg(unsigned int array[]){
-  // save some values in an array and calculate avarage to flatten values
-  unsigned int sum = 0;
-  for(int i=0; i<AVG_LENGTH; i++){
-    sum += array[i];
-  }
-  double value = sum * 1.0 / AVG_LENGTH;
-  return round(value);
-}
+byte value_tone[2] = {0, 0}; //calculated value -> to scale in volt: value*1/12
 
 void updateEncoder(){
   long newPosition = enc.read();
@@ -64,82 +51,134 @@ void updateEncoder(){
 }
 
 void updateControls(){
-  // read and flattern poti values
-  range_AVG[range_AVG_pointer] = (unsigned int) map(analogRead(PIN_RANGE), 0, 1023, 0, 5*picked_scale.length);
-  range_AVG_pointer = ++range_AVG_pointer % AVG_LENGTH;
-  range_semitones = avg(range_AVG);
-  possibility_AVG[possibility_AVG_pointer] = (unsigned int) analogRead(PIN_POSSIBILITY);
-  possibility_AVG_pointer = ++possibility_AVG_pointer % AVG_LENGTH;
-  possibility = avg(possibility_AVG);
-
-  timer_mode = CLK_MODE; // TODO: Switch
+  timer_mode[0] = CLK_MODE; // TODO: Switch
 
   updateEncoder(); // mach was damit... TODO
 
   /* set bpm TODO: neue Formel...
-    float tmp = (65536 - 3750000.0/bpm);
-    countsToInterrupt = (int) tmp;
+    bpm = ...;
+    float tmp = 60000/bpm;
+    millisToInterrupt = (int) tmp;
   */
 }
 
-byte getRandomNote(){
-  byte tone = random(0, range_semitones);
-  tone = picked_scale.values[tone % picked_scale.length] + tone / picked_scale.length * 12;
-  return tone;
+byte getRandomNote(byte channel){
+  byte tone = random(0, range_semitones[channel]);
+  return picked_scale[channel].values[tone % picked_scale[channel].length] + tone / picked_scale[channel].length * 12;
 }
 
-void generateNextStep(){ // TODO: Separater Step für output 2
+byte getRandomDiff(){
+    byte rnd = random(0,10);
+    byte diff = 0;
+    switch(rnd){
+      case 0:
+        diff = -2;
+        break;
+      case 1:
+      case 2:
+        diff = -1;
+        break;
+      case 3:
+        diff = 0;
+        break;
+      case 4:
+      case 5:
+      case 6:
+        diff = 1;
+        break;
+      case 7:
+      case 8:
+        diff = 2;
+        break;
+      case 9:
+        diff = 3;
+        break;
+      default:
+        diff = 0;
+        break;
+    }
+    return diff;
+}
+
+void generateNextStep(byte channel){ // TODO: Separater Step für output 2
   // set value_out
 
-  if(play_mode == RANDOM){
-    random_step_pointer = ++random_step_pointer % NUM_OF_STEPS;
-    if(random(0, 1023) <= possibility){
-      random_steps[random_step_pointer] = getRandomNote();
+  if(play_mode[channel] == RANDOM){
+    random_step_pointer[channel] = (random_step_pointer[channel] + 1) % NUM_OF_STEPS;
+    if(random(0, 1023) <= possibility[channel]){
+      if(channel == 0){
+        random_steps0[random_step_pointer[channel]] = getRandomNote(channel);
+        value_tone[channel] = random_steps0[random_step_pointer[channel]];
+      }else{
+        random_steps1[random_step_pointer[channel]] = getRandomNote(channel);
+        value_tone[channel] = random_steps1[random_step_pointer[channel]];
+      }
     }
-    value_tone = random_steps[random_step_pointer];
     
-  } else if(play_mode == UP){
-    scale_pointer = ++scale_pointer % range_semitones;
-    value_tone = picked_scale.values[scale_pointer % picked_scale.length] + 12 * (scale_pointer / picked_scale.length);
-  } else if(play_mode == DOWN){
+  } else if(play_mode[channel] == UP){
+    scale_pointer[channel] = (scale_pointer[channel] + 1) % range_semitones[channel];
+    value_tone[channel] = picked_scale[channel].values[scale_pointer[channel] % picked_scale[channel].length]
+        + 12 * (scale_pointer[channel] / picked_scale[channel].length);
+  }
+  else if(play_mode[channel] == DOWN){
     if(scale_pointer == 0)
-      scale_pointer = range_semitones-1;
+      scale_pointer[channel] = range_semitones[channel]-1;
     else
-      --scale_pointer;
-    value_tone = picked_scale.values[scale_pointer % picked_scale.length] + 12 * (scale_pointer / picked_scale.length);
-  } else if(play_mode == INPUT){
-    byte tone = map(analogRead(PIN_CV_INPUT1), 0, 1023, 0, 5*picked_scale.length);
-    value_tone = picked_scale.values[tone % picked_scale.length] + 12 * (tone / picked_scale.length);
+      --scale_pointer[channel];
+    value_tone[channel] = picked_scale[channel].values[scale_pointer[channel] % picked_scale[channel].length]
+        + 12 * (scale_pointer[channel] / picked_scale[channel].length);
+  }
+  else if(play_mode[channel] == SLOWLY_UP){
+    byte diff = getRandomDiff();
+    byte pointer = scale_pointer[channel] + diff;
+    if(pointer <0 || pointer > range_semitones[channel]){
+      pointer = 0;
+    }
+    scale_pointer[channel] = pointer % range_semitones[channel];
+    value_tone[channel] = picked_scale[channel].values[scale_pointer[channel] % picked_scale[channel].length]
+        + 12 * (scale_pointer[channel] / picked_scale[channel].length);
+  }
+  else if(play_mode[channel] == SLOWLY_DOWN){
+    // TODO: copy slowly up (inverted)
+  }
+  else if(play_mode[channel] == SLOWLY_BOUNCE){
+    // TODO: copy with up or down mode + globale variable
+  }
+  else if(play_mode[channel] == INPUT){
+    byte tone = 0;
+    if(channel == 0){
+      tone = map(analogRead(PIN_CV_INPUT0), 0, 1023, 0, 5*picked_scale[channel].length);
+    } else {
+      tone = map(analogRead(PIN_CV_INPUT1), 0, 1023, 0, 5*picked_scale[channel].length);
+    }
+    value_tone[channel] = picked_scale[channel].values[tone % picked_scale[channel].length] + 12 * (tone / picked_scale[channel].length);
   }
 }
 
-void writeVoltage(){
+void writeVoltage(byte channel){
   // O/P Voltage = (Reference Voltage / Resolution) x Digital Value
   // O/P Voltage = (5/ 4096) x 2048 = 2.5V
-  unsigned int value_write = round(value_tone*1024/15.0); // *4096 for dac-scale, :5 for 5v, :12 for 12 semitones per octave
+  unsigned int value_write = round(value_tone[channel]*1024/15.0); // *4096 for dac-scale, :5 for 5v, :12 for 12 semitones per octave
   
-  if(stepMarked1){
+  if(channel == 0)
+    dac0.setVoltage(value_write, false);
+  else
     dac1.setVoltage(value_write, false);
-    stepMarked1 = false;
-  }
-  if(stepMarked2){
-    dac2.setVoltage(value_write, false); // TODO: value_write duplizierien!
-    stepMarked2 = false;
-  }
+  stepMarked[channel] = false;
 }
 
 // Input signal interrupt method
-void play_tone1(){
-  if(timer_mode == TRIG_MODE){
-    if(digitalRead(PIN_TRIG1) == HIGH){
-      stepMarked1 = true;
+void play_tone0(){
+  if(timer_mode[0] == TRIG_MODE){
+    if(digitalRead(PIN_TRIG0) == HIGH){
+      stepMarked[0] = true;
     }
   }
 }
-void play_tone2(){
-  if(timer_mode == TRIG_MODE){
+void play_tone1(){
+  if(timer_mode[1] == TRIG_MODE){
     if(digitalRead(PIN_TRIG1) == HIGH){
-      stepMarked2 = true;
+      stepMarked[1] = true;
     }
   }
 }
@@ -148,40 +187,27 @@ void setup() {
   Serial.begin(9600);
   Serial.println("das gibts nicht");
 
-  for(int i=0; i<AVG_LENGTH; i++){
-    possibility_AVG[i] = (unsigned int) analogRead(PIN_POSSIBILITY);
-    range_AVG[i] = (unsigned int) map(analogRead(PIN_RANGE), 0, 1023, 0, 5*picked_scale.length);
-  }
   for(int i=0; i<NUM_OF_STEPS; i++){
-    random_steps[i] = getRandomNote();
+    random_steps0[i] = getRandomNote(0);
+    random_steps1[i] = getRandomNote(1);
   }
   //updateControls();
   
   Wire.begin(); // begins I2C communication
+  attachInterrupt(digitalPinToInterrupt(PIN_TRIG0), play_tone0, CHANGE);
   attachInterrupt(digitalPinToInterrupt(PIN_TRIG1), play_tone1, CHANGE);
-  //attachInterrupt(digitalPinToInterrupt(PIN_TRIG2), play_tone2, CHANGE);
 
+  dac0.begin(MCP4725_0);
   dac1.begin(MCP4725_1);
-  //dac2.begin(MCP4725_2);
-
-  /*
-  noInterrupts();
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCNT1 = 0;
-  TCCR1B |= (1 << CS12);  // prescale 256
-  TIMSK1 |= (1 << TOIE1);
-  interrupts();
-  */
 
   Serial.println("fertig");
 
   // TODO: löschen/ zum Testen
-  play_mode = UP;
-  timer_mode = TRIG_MODE; // bpm = 130;
-  picked_scale = scales[3];
-  range_semitones = 10;
-  possibility = 900;
+  play_mode[0] = UP;
+  timer_mode[0] = TRIG_MODE; // bpm = 130;
+  picked_scale[0] = scales[3];
+  range_semitones[0] = 10;
+  possibility[0] = 900;
 }
 
 void loop() {
@@ -194,23 +220,18 @@ void loop() {
     if(timeNow > timeForNextStep){
       timeForNextStep += millisToInterrupt;
       // TODO: abhängig von der Wahl: Mark setzen
-      stepMarked1 = true;
-      stepMarked2 = true;
+      stepMarked[0] = true;
+      stepMarked[1] = true;
     }    
   }
   
-  if(stepMarked1){
-    writeVoltage();
-    generateNextStep();
-  }
-  
-}
 
-/*
-ISR(TIMER1_OVF_vect){
-  TCNT1 = countsToInterrupt;
-  writeVoltage(1);
-  generateNextStep();
-  Serial.println("Hallo");
+  if(stepMarked[0] || play_mode[0] == INPUT){
+    writeVoltage(0);
+    generateNextStep(0);
+  }
+  if(stepMarked[1] || play_mode[1] == INPUT){
+    writeVoltage(1);
+    generateNextStep(1);
+  } 
 }
-*/
--- src/settings.h
+++ src/settings.h
@@ -4,20 +4,46 @@
 // (I2C) SDA A4
 #define PIN_SCALE A3 // TODO: in Rotary umwandeln
 
-#define PIN_CLK_INPUT2 A3
-#define PIN_CLK_INPUT1 A2
-#define PIN_CV_INPUT2 A1
-#define PIN_CV_INPUT1 A0
+#define PIN_CLK_INPUT1 A3
+#define PIN_CLK_INPUT0 A2
+#define PIN_CV_INPUT1 A1
+#define PIN_CV_INPUT0 A0
 
 // switch - settings 1 and 2
 
-#define PIN_TRIG1 2
-#define PIN_TRIG2 3
+#define PIN_TRIG0 2
+#define PIN_TRIG1 3
 
 // rotary encoder
-#define PIN_ENC_1 4
-#define PIN_ENC_2 5
+#define PIN_ENC_0 4
+#define PIN_ENC_1 5
 
 #define PIN_MENU_TRIGGER 6
 
 // TODO: trigger menu/option/channel
+
+
+/*
+Potis:
+    possibility
+    range (steps)
+    scale
+    tone mode (incl. input)
+    random/save length
+    speed/ shift(?)
+
+Buttons:
+    set1
+    set2
+    record (over random array)
+
+Jacks in:
+    trigger1
+    trigger2
+    cv_in1
+    cv_in2
+
+Jacks out:
+    out1
+    out2
+*/
